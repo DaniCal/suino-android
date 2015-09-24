02a8021d0e8d93840c41dc46ccbd454abbdc4f84
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2015-09-24 11:52:22 +0200

    initial commit

---
+++ app/src/main/java/borell/com/suino/SuinoFragment.java
@@ -0,0 +1,23 @@
+package borell.com.suino;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SuinoFragment extends Fragment {
+
+    public SuinoFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_suino, container, false);
+    }
+}
