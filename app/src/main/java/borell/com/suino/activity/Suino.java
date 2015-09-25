dd8dd76641b82623a3503d2f5ca35da3f038e679
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2015-09-25 09:46:30 +0200

    NavigationMenu added

--- app/src/main/java/borell/com/suino/activity/Suino.java
+++ app/src/main/java/borell/com/suino/activity/Suino.java
@@ -5,22 +5,29 @@ import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.security.MessageDigest;
 
+import borell.com.suino.fragment.FragmentDrawer;
 import borell.com.suino.fragment.LoginFragment;
 import borell.com.suino.R;
+import borell.com.suino.fragment.SuinoFragment;
 
 
-public class Suino extends AppCompatActivity {
+public class Suino extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener{
 
-    private LoginFragment loginFragment;
+    private FragmentDrawer drawerFragment;
+//    private LoginFragment loginFragment;
     private Toolbar mToolbar;
 
     @Override
@@ -34,12 +41,18 @@ public class Suino extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
+        displayView(0);
+
     }
 
     @Override
     public void onAttachFragment(Fragment fragment) {
         super.onAttachFragment(fragment);
-        loginFragment  = (LoginFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_login);
+//        loginFragment  = (LoginFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_login);
     }
 
     @Override
@@ -79,6 +92,38 @@ public class Suino extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
+
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new SuinoFragment();
+                title = getString(R.string.title_home);
+                break;
+            case 1:
+                fragment = new LoginFragment();
+                title = getString(R.string.title_friends);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
     }
 
 
